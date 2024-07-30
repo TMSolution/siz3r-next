4b51a0230f3a7ca7ed271ee5d57dcff30e589df2
Robert Łoziński <robert.lozinski@gmail.com>
2024-07-30 15:57:13 +0200

    Update actions.js

--- app/actions.js
+++ app/actions.js
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 export async function changeLanguage(lang) {
   return new Promise((resolve) => {
     //cookies().set("lang", lang);
+    console.debug("Setting language to " + lang);
     cookies().set({
       name: "lang",
       value: lang,
