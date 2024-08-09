fafa81bc1fcabc2f1035dfb7d423a24026e1f6c4
Robert Łoziński <robert.lozinski@gmail.com>
2024-08-09 11:33:41 +0200

    Footer with translations

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,22 @@
-import { Box, Divider, Grid, IconButton } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 import Block from "./Layout/Block";
 import Component from "./Layout/Component";
 import Image from "./Layout/Image";
-import Text from "./Layout/Text";
 import {
   Facebook,
   Instagram,
   Twitter,
   YouTube,
   LinkedIn,
+  Language,
 } from "@mui/icons-material";
 import { ArrowDown, ChevronDown, Play } from "lucide-react";
 import Link from "next/link";
@@ -26,9 +34,9 @@ export default function Footer({ isMobile }) {
     { icon: YouTube, href: "https://youtube.com" },
   ];
   const links = [
-    { label: "Home", href: "/" },
-    { label: "Dla biznesu", href: "/business" },
-    { label: "Kontakt", href: "/contact" },
+    { label: dictionary.footer.home, href: "/" },
+    { label: dictionary.footer.business, href: "/business" },
+    { label: dictionary.footer.contact, href: "/contact" },
   ];
   return (
     <Block
@@ -38,14 +46,15 @@ export default function Footer({ isMobile }) {
 
         position: "relative",
       }}
-      gridProps={{ spacing: 3, paddingTop: 3, paddingBottom: 3 }}>
+      gridProps={{ spacing: 3, paddingTop: 3, paddingBottom: 0 }}>
       <Image
         md={12}
         xs={12}
         src="/logo.png"
-        style={{ maxWidth: 250, alignSelf: "center",paddingBottom:1 }}
+        style={{ maxWidth: 250, alignSelf: "center", paddingBottom: 1 }}
         gridStyle={{ display: "flex" }}
       />
+
       <Component
         md={12}
         xs={12}
@@ -131,6 +140,51 @@ export default function Footer({ isMobile }) {
             </Button>
           </Link>
         ))}
+      </Component>
+      <Component
+        xs={12}
+        md={12}
+        gridStyle={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}>
+        <Select
+          onChange={(e) => {
+            console.log(e.target.value);
+            changeLanguage(e.target.value).then(() => {
+              window.location.replace(window.location.href);
+            });
+          }}
+          value={lang}
+          size="small"
+          sx={{
+            width: { xs: "100%", md: "unset" },
+            ".MuiSelect-select": {
+              paddingLeft: { xs: undefined, md: 1 },
+              paddingRight: { xs: undefined, md: 1 },
+            },
+          }}
+          renderValue={(value) => (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}>
+              <Language />
+              <Typography style={{ paddingLeft: 8, paddingRight: 4 }}>
+                {languages[value]}
+              </Typography>
+            </div>
+          )}>
+          {Object.entries(languages).map((item) => (
+            <MenuItem key={item[0]} value={item[0]}>
+              {item[1]}
+            </MenuItem>
+          ))}
+        </Select>
       </Component>
       <Grid xs={12} container item direction={"row"}>
         <Divider style={{ width: "100%" }} />
